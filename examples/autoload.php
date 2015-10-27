74b156b5626b6721d78280e94615cdb9fa7926ce
Morten Mehus <morten.mehus@gmail.com>
2015-10-28 00:31:49 +0100

    autoloader

--- examples/autoload.php
+++ examples/autoload.php
@@ -3,6 +3,7 @@ spl_autoload_register(function($name){
     if (!defined('DS')) {
         define('DS', DIRECTORY_SEPARATOR);
     }
+    // Please change this according to your file structure
     $pathtoautoclass = dirname(__FILE__).DS."class";
     $class = $pathtoautoclass. DS .str_replace("\\", DS, $name);
     $file = $class.".php";
